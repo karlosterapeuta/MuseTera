a0e4652118a690a73566a9228e6ab941de4d68ba
karlosterapeuta <musicoterapeutakarlos@gmail.com>
2025-01-19 00:44:51 -0300

    fix: adiciona favicon e atualiza metadata

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,4 +3,7 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'MuseTera',
   description: 'Sistema de Gestão para Musicoterapeutas',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
