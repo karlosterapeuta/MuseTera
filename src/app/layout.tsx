fa49386bfa1ca18cc4c148baf7b179d30da253c8
karlosterapeuta <musicoterapeutakarlos@gmail.com>
2025-01-19 07:26:50 -0300

    fix: simplifica layout principal e remove LayoutClient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/Toast'
 import { Providers } from '@/components/providers'
-import { LayoutClient } from '@/components/LayoutClient'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,10 +25,8 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Providers>
-          <LayoutClient>
-            <Toaster />
-            {children}
-          </LayoutClient>
+          <Toaster />
+          {children}
         </Providers>
       </body>
     </html>
